02e8e7fb0755c379e47109790da1a1db0c8139b0
basharat1992 <basharathussain05@gmail.com>
2019-02-09 17:45:55 +0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -80,66 +80,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		<div class="content_top_img">
 			<img src="images/content_top_img.jpg" alt="" />
 		</div>
-		<div class="content_top_data">
-			<h2>WELCOME TO OUR <span>COMPANY </span></h2>
-			<h3>We can provide different types of marbles</h3>
-			<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-		</div>
-	<div class="clear"></div>
-	</div>
-	<div class="content_bottom">
-		<h2>FEATURED HOMES</h2>
-		<div class="more">
-			<p>more <img src="images/more.png" alt="" /></p>
-		</div>
-		<div class="grides">
-			<div class="grid1">
-				<img src="images/img_featured1.jpg" alt="" />
-				<div class="grid_top">
-					<h4>4 Marbles</h4>
-					<h5>Price <span>PKR 10,000</span></h5>
-				</div>
-			<div class="clear"></div>
-				<div class="grid_data">
-					<h3>It is a marble related website</h3>
-					<p>You can find here marbles and granites of different type</p>
-				</div>
-			</div>
-		<div class="grid2">
-		<img src="images/img_featured2.jpg" alt="" />
-		<div class="grid_top">
-		<h4>6 Marbles</h4>
-		<h5>Price <span>PKR 30,000</span></h5>
-		</div>
-		<div class="clear"></div>
-		<div class="grid_data">
-		<h3>We can provide each type of marbles</h3>
-		<p>Each of the product we are offering have quality</p>
-		</div>
-		</div>
-		<div class="clear"></div>
-		</div>
-	</div>
-</div>
-<div class="clear"></div>
-</div>
-<div class="footer">
-		<div class="footer_nav">
-			<ul>
-				<li><a href="#">Home</a></li>
-				<li><a href="#">About</a></li>
-				<li><a href="#">Selling</a></li>
-				<li><a href="#">Buying</a></li>
-				<li><a href="#">Services</a></li>
-				<li><a href="#">Contacts</a></li>
-			</ul>
-		</div>
-	<div class="copy_right">
-	<p>© Pak Ideal Inc. All Rights Reserved.</p>
-	</div>
-	<div class="clear"></div>
-	</div>
-</div>
+
 </body>
 </html>
 
